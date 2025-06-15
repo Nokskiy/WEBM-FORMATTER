c6ffb7bc21694bfb98fa3cb247d95010aea421cd
Nokskiy <tsfkafsu@yandex.ru>
2025-06-15 17:51:03 +0500

    Fix

--- Converter.cs
+++ Converter.cs
@@ -7,19 +7,34 @@ public static class Converter
 {
     public static int Convert(string path, int frameRate = 30)
     {
-        if (path == null) return 1;
+        if (path == null || !File.Exists(path))
+        {
+            Console.WriteLine("File not exists");
+            return 1;
+        }
 
         var ffMpeg = new FFMpegConverter();
+        string outputPath = Path.Combine(
+            Path.GetDirectoryName(path)!,
+            Path.GetFileNameWithoutExtension(path) + ".webm");
 
-        var outputOptions = new ConvertSettings();
-        outputOptions.VideoCodec = "libvpx-vp9"; // Use VP9 for WebM with alpha channel support
-        outputOptions.VideoFrameRate = frameRate;
+        var outputOptions = new ConvertSettings
+        {
+            VideoCodec = "libvpx-vp9",
+            VideoFrameRate = frameRate,
+            CustomOutputArgs = "-pix_fmt yuva420p -b:v 500k -crf 30 -quality good -cpu-used 4 -row-mt 1 -t 3",
+        };
 
-        string outputPath = Path.GetDirectoryName(path) + @"\" + Path.GetFileNameWithoutExtension(path) + ".WEBM";
-
-        Console.WriteLine("Output - " + outputPath);
-
-        ffMpeg.ConvertMedia(path, null, outputPath, Format.webm, outputOptions);
-        return 0;
+        try
+        {
+            ffMpeg.ConvertMedia(path, null, outputPath, Format.webm, outputOptions);
+            Console.WriteLine($"Output:: {outputPath}");
+            return 0;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+            return 1;
+        }
     }
 }
